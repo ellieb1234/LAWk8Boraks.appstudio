d40b4bc67f5e64a1beab485db1032ee1fdf4ddc9
ellieb1234 <ellieboraks@creighton.edu>
2021-04-02 15:20:45 -0500

    favExercises

---
+++ LAWk8Boraks.appstudio/forms/favExercises/favExercises.js
@@ -0,0 +1,26 @@
+var exercises = ["pullups", "pushups", "situps", "jogging", "plank", "curls"]
+var coreExercises = ["situps", "plank"]
+var responses = []
+
+favExercises.onshow = function() {
+      selExercises.clear()
+      for (let y = 0; y<exercises.length; y++) {
+            selExercises.addItem(exercises[y])
+      }
+}
+
+selExercises.onclick = function() {
+      let val=selExercises.text
+      responses.push(val)
+      console.log(responses)
+}
+
+btnSubmit.onclick = function() {
+      responses.sort();
+      if (responses[1] == coreExercises[0] && responses[0] == coreExercises[1]) {
+            lblExercises.value='You chose situps and planks - those are the two core exercises.'
+      } else {
+            lblExercises.value='You did not pick the two core exercises.'
+            }
+      responses=[]
+}
